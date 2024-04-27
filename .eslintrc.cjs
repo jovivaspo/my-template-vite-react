af577122b2124588aeb7c529e4c3330ff9d67e24
jovivaspo <jorgevipo.dev@gmail.com>
2024-04-27 09:35:06 +0200

    Configure husky + lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,27 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    camelcase: [
+      'error',
+      {
+        properties: 'always',
+      },
+    ],
+    'no-console': 'error',
+    'no-magic-numbers': ['error', { ignoreArrayIndexes: true }],
+    'no-return-await': 'error',
+    'max-lines': [
       'warn',
-      { allowConstantExport: true },
+      {
+        max: 300,
+        skipBlankLines: true,
+      },
     ],
   },
-}
+};
